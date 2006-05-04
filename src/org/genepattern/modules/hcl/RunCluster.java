687c8d95ae60d2d1f2f1e5dd1b2174678b3329ab
jgould <jgould@47fccdf2-e235-0410-87e5-63258e95d6e5>
2006-05-04 21:35:53 +0000

    Updated imports
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/HierarchicalClustering@8681 47fccdf2-e235-0410-87e5-63258e95d6e5

--- src/org/genepattern/modules/hcl/RunCluster.java
+++ src/org/genepattern/modules/hcl/RunCluster.java
@@ -1,50 +1,52 @@
 /*
-  The Broad Institute
-  SOFTWARE COPYRIGHT NOTICE AGREEMENT
-  This software and its documentation are copyright (2003-2006) by the
-  Broad Institute/Massachusetts Institute of Technology. All rights are
-  reserved.
+ The Broad Institute
+ SOFTWARE COPYRIGHT NOTICE AGREEMENT
+ This software and its documentation are copyright (2003-2006) by the
+ Broad Institute/Massachusetts Institute of Technology. All rights are
+ reserved.
 
-  This software is supplied without any warranty or guaranteed support
-  whatsoever. Neither the Broad Institute nor MIT can be responsible for its
-  use, misuse, or functionality.
-*/
-
+ This software is supplied without any warranty or guaranteed support
+ whatsoever. Neither the Broad Institute nor MIT can be responsible for its
+ use, misuse, or functionality.
+ */
 
 package org.genepattern.modules.hcl;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Target;
 import org.apache.tools.ant.taskdefs.Chmod;
 import org.apache.tools.ant.taskdefs.Execute;
-import org.apache.tools.ant.Target;
-import org.apache.tools.ant.Project;
-
-import java.io.*;
-import java.util.*;
-
-import org.genepattern.io.expr.stanford.StanfordTxtWriter;
-import org.genepattern.io.expr.IExpressionDataReader;
 import org.genepattern.data.expr.ExpressionData;
+import org.genepattern.io.Util;
+import org.genepattern.io.expr.IExpressionDataReader;
+import org.genepattern.io.expr.stanford.StanfordTxtWriter;
 import org.genepattern.module.AnalysisUtil;
-import org.genepattern.ioutil.Util;
 
 public class RunCluster {
-    
+
     public static void main(String[] args) {
         try {
-        		
-  
+
             String libdir = args[0];
             boolean chmodFlag = true;
             String executable = null;
-            if(System.getProperty("os.name").toLowerCase().startsWith("windows")) {
+            if (System.getProperty("os.name").toLowerCase().startsWith(
+                    "windows")) {
                 executable = "cluster.exe";
                 chmodFlag = false;
-            } else if(System.getProperty("mrj.version") != null) {
+            } else if (System.getProperty("mrj.version") != null) {
                 executable = "clusterMac";
             } else {
                 executable = "clusterLinux";
             }
-            if(chmodFlag) {
+            if (chmodFlag) {
                 MyChmod chmod = new MyChmod();
                 chmod.setDir(new File(libdir));
                 chmod.setIncludes(executable);
@@ -52,15 +54,15 @@ public class RunCluster {
                 chmod.execute();
             }
             args[0] = libdir + executable;
-            
+
             String inputFileName = args[2];
-            
+
             IExpressionDataReader reader = AnalysisUtil
-				.getExpressionReader(inputFileName);
-            
-            ExpressionData expressionData = AnalysisUtil.readExpressionData(reader,
-                                                                            inputFileName);
-           
+                    .getExpressionReader(inputFileName);
+
+            ExpressionData expressionData = AnalysisUtil.readExpressionData(
+                    reader, inputFileName);
+
             StanfordTxtWriter writer = new StanfordTxtWriter();
             String txtFileName = Util.getBaseFileName(inputFileName) + ".txt";
             BufferedOutputStream os = null;
@@ -68,50 +70,51 @@ public class RunCluster {
                 os = new BufferedOutputStream(new FileOutputStream(txtFileName));
                 writer.write(expressionData, os);
             } finally {
-                if(os!=null) {
-                  os.close();  
+                if (os != null) {
+                    os.close();
                 }
                 new File(txtFileName).deleteOnExit();
             }
-           
+
             args[2] = txtFileName;
             List newArgs = new ArrayList(Arrays.asList(args));
-            for(int i = 0; i < newArgs.size(); i++) {
-            	String arg = (String) newArgs.get(i);
-            	if(arg.equals("mean.row")) {
-            		newArgs.remove(i); // median.row median.col
-            		newArgs.add("-cg");
-            		newArgs.add("a");
-            		i--;
-            	} else if(arg.equals("median.row")) {
-            		newArgs.remove(i);
-            		newArgs.add("-cg");
-            		newArgs.add("m");
-            		i--;
-            	}
-            	if(arg.equals("mean.column")) {
-            		newArgs.remove(i);
-            		newArgs.add("-ca");
-            		newArgs.add("a");
-            		i--;
-            	} else if(arg.equals("median.column")) {
-            		newArgs.remove(i);
-            		newArgs.add("-ca");
-            		newArgs.add("m");
-            		i--;
-            	}
+            for (int i = 0; i < newArgs.size(); i++) {
+                String arg = (String) newArgs.get(i);
+                if (arg.equals("mean.row")) {
+                    newArgs.remove(i); // median.row median.col
+                    newArgs.add("-cg");
+                    newArgs.add("a");
+                    i--;
+                } else if (arg.equals("median.row")) {
+                    newArgs.remove(i);
+                    newArgs.add("-cg");
+                    newArgs.add("m");
+                    i--;
+                }
+                if (arg.equals("mean.column")) {
+                    newArgs.remove(i);
+                    newArgs.add("-ca");
+                    newArgs.add("a");
+                    i--;
+                } else if (arg.equals("median.column")) {
+                    newArgs.remove(i);
+                    newArgs.add("-ca");
+                    newArgs.add("m");
+                    i--;
+                }
             }
-            
+
             Execute execute = new Execute();
-            String[] s = (String[])newArgs.toArray(new String[0]);
+            String[] s = (String[]) newArgs.toArray(new String[0]);
             execute.setCommandline(s);
             execute.execute();
-        } catch(Exception e) {
-        		e.printStackTrace();
-            System.err.println("An error occurred while running the algorithm.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err
+                    .println("An error occurred while running the algorithm.");
         }
     }
-    
+
     static class MyChmod extends Chmod {
         public MyChmod() {
             project = new Project();
@@ -119,7 +122,7 @@ public class RunCluster {
             taskType = "chmod";
             taskName = "chmod";
             target = new Target();
-        }       
-    } 
-   
+        }
+    }
+
 }
