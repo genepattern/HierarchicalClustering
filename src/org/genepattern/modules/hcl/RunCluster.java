c27954cc2014c343fc4001a9582444a45c800d39
jgould <jgould@47fccdf2-e235-0410-87e5-63258e95d6e5>
2008-07-09 13:04:53 +0000

    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/HierarchicalClustering@14130 47fccdf2-e235-0410-87e5-63258e95d6e5

--- src/org/genepattern/modules/hcl/RunCluster.java
+++ src/org/genepattern/modules/hcl/RunCluster.java
@@ -15,114 +15,82 @@ package org.genepattern.modules.hcl;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.tools.ant.Project;
-import org.apache.tools.ant.Target;
-import org.apache.tools.ant.taskdefs.Chmod;
-import org.apache.tools.ant.taskdefs.Execute;
 import org.genepattern.io.DatasetParser;
 import org.genepattern.io.IOUtil;
 import org.genepattern.io.stanford.StanfordTxtWriter;
 import org.genepattern.matrix.Dataset;
 import org.genepattern.module.AnalysisUtil;
+import org.genepattern.module.ExecutableWrapper;
 
-public class RunCluster {
+public class RunCluster extends ExecutableWrapper {
+
+    public RunCluster(String[] args) {
+	super(args);
+    }
 
     public static void main(String[] args) {
-        try {
-
-            String libdir = args[0];
-            boolean chmodFlag = true;
-            String executable = null;
-            if (System.getProperty("os.name").toLowerCase().startsWith(
-                    "windows")) {
-                executable = "cluster.exe";
-                chmodFlag = false;
-            } else if (System.getProperty("mrj.version") != null) {
-                executable = "clusterMac";
-            } else {
-                executable = "clusterLinux";
-            }
-            if (chmodFlag) {
-                MyChmod chmod = new MyChmod();
-                chmod.setDir(new File(libdir));
-                chmod.setIncludes(executable);
-                chmod.setPerm("+x");
-                chmod.execute();
-            }
-            args[0] = libdir + executable;
-
-            String inputFileName = args[2];
-
-            DatasetParser reader = AnalysisUtil
-                    .getDatasetParser(inputFileName);
-
-            Dataset dataset = AnalysisUtil.readDataset(
-                    reader, inputFileName);
-
-            StanfordTxtWriter writer = new StanfordTxtWriter();
-            String txtFileName = IOUtil.getBaseFileName(inputFileName) + ".txt";
-            BufferedOutputStream os = null;
-            try {
-                os = new BufferedOutputStream(new FileOutputStream(txtFileName));
-                writer.write(dataset, os);
-            } finally {
-                if (os != null) {
-                    os.close();
-                }
-                new File(txtFileName).deleteOnExit();
-            }
-
-            args[2] = txtFileName;
-            List newArgs = new ArrayList(Arrays.asList(args));
-            for (int i = 0; i < newArgs.size(); i++) {
-                String arg = (String) newArgs.get(i);
-                if (arg.equals("mean.row")) {
-                    newArgs.remove(i); // median.row median.col
-                    newArgs.add("-cg");
-                    newArgs.add("a");
-                    i--;
-                } else if (arg.equals("median.row")) {
-                    newArgs.remove(i);
-                    newArgs.add("-cg");
-                    newArgs.add("m");
-                    i--;
-                }
-                if (arg.equals("mean.column")) {
-                    newArgs.remove(i);
-                    newArgs.add("-ca");
-                    newArgs.add("a");
-                    i--;
-                } else if (arg.equals("median.column")) {
-                    newArgs.remove(i);
-                    newArgs.add("-ca");
-                    newArgs.add("m");
-                    i--;
-                }
-            }
-
-            Execute execute = new Execute();
-            String[] s = (String[]) newArgs.toArray(new String[0]);
-            execute.setCommandline(s);
-            execute.execute();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err
-                    .println("An error occurred while running the algorithm.");
-        }
+	new RunCluster(args);
     }
 
-    static class MyChmod extends Chmod {
-        public MyChmod() {
-            project = new Project();
-            project.init();
-            taskType = "chmod";
-            taskName = "chmod";
-            target = new Target();
-        }
-    }
+    @Override
+    protected String[] createNewArgs() {
+	args[0] = libdir + executable;
+	String inputFileName = args[2];
+	DatasetParser reader = AnalysisUtil.getDatasetParser(inputFileName);
+	Dataset dataset = AnalysisUtil.readDataset(reader, inputFileName);
+	StanfordTxtWriter writer = new StanfordTxtWriter();
+	String txtFileName = IOUtil.getBaseFileName(inputFileName) + ".txt";
+	BufferedOutputStream os = null;
+	try {
+	    os = new BufferedOutputStream(new FileOutputStream(txtFileName));
+	    writer.write(dataset, os);
+	} catch (IOException x) {
+	    System.err.println("Unable to create temporary file.");
+	    System.exit(1);
+	} finally {
+	    if (os != null) {
+		try {
+		    os.close();
+		} catch (IOException e) {
+		}
+	    }
+	    new File(txtFileName).deleteOnExit();
+	}
 
+	args[2] = txtFileName;
+	List<String> newArgs = new ArrayList<String>(Arrays.asList(args));
+	for (int i = 0; i < newArgs.size(); i++) {
+	    String arg = newArgs.get(i);
+	    if (arg.equals("mean.row")) {
+		newArgs.remove(i); // median.row median.col
+		newArgs.add("-cg");
+		newArgs.add("a");
+		i--;
+	    } else if (arg.equals("median.row")) {
+		newArgs.remove(i);
+		newArgs.add("-cg");
+		newArgs.add("m");
+		i--;
+	    }
+	    if (arg.equals("mean.column")) {
+		newArgs.remove(i);
+		newArgs.add("-ca");
+		newArgs.add("a");
+		i--;
+	    } else if (arg.equals("median.column")) {
+		newArgs.remove(i);
+		newArgs.add("-ca");
+		newArgs.add("m");
+		i--;
+	    }
+	}
+
+	return newArgs.toArray(new String[0]);
+
+    }
 }
